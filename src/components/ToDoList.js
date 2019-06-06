818b03a13c0897af41b3f9f82c1cae753746ec4b
brandonngiam <brandonngiam@user.noreply.github.com>
2019-06-07 00:16:03 +0800

    Added features but pre-refactoring

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -2,15 +2,38 @@ import React from "react";
 import "../styles/ToDoList.css";
 const md5 = require("md5");
 
-function ToDoList(props) {
-  const toDoListArray = props.toDoList;
-  const JSXToDoList = toDoListArray.map(todo => (
-    <li className={props.className} key={md5(todo)}>
-      {todo}
-    </li>
-  ));
+class ToDoList extends React.Component {
+  toggleHandler = event => {
+    event.target.classList.toggle("done");
+  };
 
-  return <ul>{JSXToDoList}</ul>;
+  mouseOverHandler = event => {
+    if (event.target.nodeName !== "UL")
+      event.target.classList.add("todo-in-focus");
+  };
+
+  mouseOutHandler = event => {
+    event.target.classList.remove("todo-in-focus");
+  };
+
+  render() {
+    const toDoListArray = this.props.toDoList;
+    const JSXToDoList = toDoListArray.map(todo => (
+      <li className={this.props.className} key={md5(todo)}>
+        {todo}
+      </li>
+    ));
+
+    return (
+      <ul
+        onClick={this.toggleHandler}
+        onMouseOver={this.mouseOverHandler}
+        onMouseOut={this.mouseOutHandler}
+      >
+        {JSXToDoList}
+      </ul>
+    );
+  }
 }
 
 export default ToDoList;
